4f9184ab7a0b25f9941df2974042bd3c29690f1f
Alex Molnar <alex.molnar@ing.com>
2026-08-06 09:31:19 +0200

    Added template to create new database with dedicated user

---
+++ scripts/create-dabase.sql
@@ -0,0 +1,25 @@
+-- rename every NEW_NAME instance to the name you intend to use
+-- Change PASSWORD to the password you want to use for the new user
+-- Connect as superuser (postgres)
+CREATE DATABASE "NEW_NAME";
+CREATE USER "NEW_NAME" WITH PASSWORD 'PASSWORD';
+GRANT ALL PRIVILEGES ON DATABASE "NEW_NAME" TO "NEW_NAME";
+EXECUTE format('REVOKE CONNECT ON DATABASE NEW_NAME FROM PUBLIC');
+EXECUTE format('GRANT CONNECT ON DATABASE NEW_NAME TO postgres');
+
+DO $$
+DECLARE
+    db_name TEXT;
+BEGIN
+    FOR db_name IN 
+        SELECT datname FROM pg_database 
+        WHERE datname != 'NEW_NAME'
+    LOOP
+        EXECUTE format('REVOKE CONNECT ON DATABASE %I FROM "NEW_NAME"', db_name);
+    END LOOP;
+END $$;
+
+-- psql -U postgres -d NEW_NAME
+GRANT ALL ON SCHEMA public TO "NEW_NAME";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO "NEW_NAME";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO "NEW_NAME";
